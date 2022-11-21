866b3b6f849c21c3b35a01ba829b8eca617bbf83
forrestmorrison <forrestmorrison@gmail.com>
2022-11-21 17:52:51 -0600

    added getPostsByUserId to postsSlice

--- client/src/features/posts/postsSlice.js
+++ client/src/features/posts/postsSlice.js
@@ -35,6 +35,30 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
                 ...result.ids.map(id => ({ type: 'Post', id }))
             ]
         }),
+        getPostsByUserId: builder.query({
+            query: id => `/posts/?.userId=${id}`,
+            transformResponse: responseData => {
+                let min = 1;
+                const loadedPosts = responseData.map(post => {
+                    if (!post?.date) post.date = sub(new Date(), { minutes: min++ }).toISOString();
+                    if (!post?.reactions) post.reactions = {
+                        thumbsUp: 0,
+                        wow: 0,
+                        heart: 0,
+                        rocket: 0,
+                        coffee: 0
+                    }
+                    return post;
+                });
+                return postsAdapter.setAll(initialState, loadedPosts)
+            },
+            providesTags: (result, error, arg) => {
+                console.log(result)
+                return [
+                    ...result.ids.map(id => ({ type: 'Post', id }))
+                ]
+            }
+        })
     })
 })
 
