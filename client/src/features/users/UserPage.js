72a585e2bdbd0a4595d896b34acf8de77f2a5152
forrestmorrison <forrestmorrison@gmail.com>
2022-11-22 17:40:52 -0600

    updated UserPage

--- client/src/features/users/UserPage.js
+++ client/src/features/users/UserPage.js
@@ -15,17 +15,23 @@ const UserPage = () => {
         error
     } = useGetPostsByUserIdQuery(userId);
 
-    const postTitles = postsForUser.map(post => (
-        <li key={post.id}>
-            <Link to={`/post/${post.id}`}>{post.title}</Link>
-        </li>
-    ))
+    let content;
+    if (isLoading) {
+        const { ids, entities } = postsForUser
+        content = ids.map(id => (
+            <li key={id}>
+                <Link to={`/post/${id}`}>{entities[id].title}</Link>
+            </li>
+        ))
+    } else if (isError) {
+        content = <p>{error}</p>;
+    }
 
     return (
         <section>
             <h2>{user?.name}</h2>
 
-            <ol>{postTitles}</ol>
+            <ol>{content}</ol>
         </section>
     )
 }
