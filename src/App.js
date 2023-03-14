0819bdcde462b718d6ef5b10ecb1f2be3401acb7
forrestmorrison <forrestmorrison@gmail.com>
2023-03-14 17:54:18 -0500

    added Sign In with Google

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import CreatePost from "./pages/CreatePost";
 
 function App() {
+
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/createpost" element={<CreatePost />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <nav>
+          <Link to="/"> Home </Link>
+          <Link to="/createpost"> Create New Post </Link>
+          <Link to="/login"> Log In </Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login setIsAuth={setIsAuth}/>} />
+          <Route path="/createpost" element={<CreatePost />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react"
+import { auth, provider } from "../firebaseConfig"
+import { signInWithPopup } from "firebase/auth"
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
+const Login = ({setIsAuth}) => {
+
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, provider).then((result) => {
+            localStorage.setItem("isAuth", true);
+            setIsAuth(true);
+        })
+    }
+
+    return (
+        <div className="page-content">
+            <p>Sign in with Google to continue.</p>
+            <button onClick={signInWithGoogle}>
+                sign in with Google
+            </button>
+        </div>
+    )
 }
 
 export default Login
