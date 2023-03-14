1d52e7873427e509e2f07a5c0ebcd8d0cfc89252
forrestmorrison <forrestmorrison@gmail.com>
2023-03-14 17:17:08 -0500

    added React Router routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
-    <main className="App">
-      Hello App
-    </main>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/createpost" element={<CreatePost />} />
+      </Routes>
+    </Router>
   );
 }
 
