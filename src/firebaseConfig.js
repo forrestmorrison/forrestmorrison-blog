380081f2830d455bec37142b2dc0d808e88565a5
forrestmorrison <forrestmorrison@gmail.com>
2023-03-14 17:01:21 -0500

    connected app to Firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBjBlEWkHuElPp0jKMzoThE5Tyxa2nb1GY",
+  authDomain: "forrestmorrison-blog.firebaseapp.com",
+  projectId: "forrestmorrison-blog",
+  storageBucket: "forrestmorrison-blog.appspot.com",
+  messagingSenderId: "158992550830",
+  appId: "1:158992550830:web:58be64748c4909044a563f",
+  measurementId: "G-S220E3W5P5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
