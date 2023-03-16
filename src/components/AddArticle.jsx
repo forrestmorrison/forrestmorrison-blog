cef509c53e35142d6a7104cb99bca70bc57ffbf0
forrestmorrison <forrestmorrison@gmail.com>
2023-03-15 21:14:39 -0500

    set up AddArticle component

--- src/components/AddArticle.jsx
+++ src/components/AddArticle.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import { Timestamp } from 'firebase/firestore'
+import { useState } from 'react'
+import { ref } from 'firebase/storage'
 
 const AddArticle = () => {
-  return (
-    <div>AddArticle</div>
-  )
+    const [formData, setFormData] = useState({
+        title: "",
+        description: "",
+        image: "",
+        createdAt: Timestamp.now().toDate()
+    })
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const handleImageChange = (e) => {
+        setFormData({ ...formData, image: e.target.files[0] })
+    }
+
+    const handlePublish = () => {
+        if(!formData.title || !formData.description || !formData.image) {
+            alert("Please fill out all fields")
+            return;
+        }
+
+        const storageRef = ref
+    }
+
+    return (
+        <div className="border p-3 mt-3 bg-light" style={{ position: "fixed" }}>
+            <h2>Create Article</h2>
+            <label htmlFor="">Title</label>
+            <input 
+                type="text" 
+                name="title" 
+                className="form-control" 
+                value={formData.title} 
+                onChange={(e) => handleChange(e)}
+            />
+
+            <label htmlFor="">Description</label>
+            <textarea 
+                name="description" 
+                className="form-control" 
+                value={formData.description}
+                onChange={(e) => handleChange(e)}
+            />
+
+            <label htmlFor="">Image</label>
+            <input 
+                type="file" 
+                name="image" 
+                accept="image/*" 
+                className="form-control"
+                onChange={(e) => handleImageChange(e)}
+            />
+
+            <div className="progress mt-2">
+                <div className="progress-bar progress-bar-striped" style={{ width: "50%" }}>
+                    50%
+                </div>
+            </div>
+
+            <button className="form-control btn btn-primary mt-2" onClick={handlePublish}>Publish</button>
+        </div>
+    )
 }
 
 export default AddArticle
