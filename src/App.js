954d1860550749db463fd8ce887ec2c2ef08776d
forrestmorrison <forrestmorrison@gmail.com>
2023-03-15 19:52:00 -0500

    installed dependencies, setup Firebase

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div>
-      Hello world!
+      Blog App
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCEVURguH7PAQ26JapCshZHgmJSIfwnpfM",
+  authDomain: "forrests-blog.firebaseapp.com",
+  projectId: "forrests-blog",
+  storageBucket: "forrests-blog.appspot.com",
+  messagingSenderId: "858260145478",
+  appId: "1:858260145478:web:8255abd3767adcbb981147"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
